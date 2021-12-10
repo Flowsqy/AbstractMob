d40274a4df387f3d9633e049b55cc598014c7ffd
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2021-12-10 22:59:25 +0100

    Init project

---
+++ src/main/java/fr/flowsqy/abstractmob/AbstractMobPlugin.java
@@ -0,0 +1,12 @@
+package fr.flowsqy.abstractmob;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AbstractMobPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
