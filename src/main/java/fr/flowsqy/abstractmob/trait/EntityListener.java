7cab78fe66b0c9e8abeca9270d23000f743f6271
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2022-01-16 21:16:40 +0100

    Add random for lightning effect

--- src/main/java/fr/flowsqy/abstractmob/trait/EntityListener.java
+++ src/main/java/fr/flowsqy/abstractmob/trait/EntityListener.java
@@ -1,5 +1,6 @@
 package fr.flowsqy.abstractmob.trait;
 
+import fr.flowsqy.abstractmob.AbstractMobPlugin;
 import fr.flowsqy.abstractmob.key.Keys;
 import org.bukkit.Sound;
 import org.bukkit.SoundCategory;
@@ -14,13 +15,35 @@ import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.metadata.MetadataValue;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 public class EntityListener implements Listener {
+
+    private final AbstractMobPlugin plugin;
+    private final Random random;
+
+    public EntityListener(AbstractMobPlugin plugin) {
+        this.plugin = plugin;
+        random = new Random();
+    }
 
     @EventHandler(priority = EventPriority.MONITOR)
     private void onDeath(EntityDeathEvent event) {
         final LivingEntity entity = event.getEntity();
-        if (entity.hasMetadata(Keys.LIGHTNING_ON_DEATH.getKey())) {
+        final List<MetadataValue> values = entity.getMetadata(Keys.LIGHTNING_ON_DEATH.getKey());
+        if (values.isEmpty()) {
+            return;
+        }
+        final Optional<MetadataValue> pluginValue = values.stream()
+                .filter(value -> value.getOwningPlugin() == plugin)
+                .findAny();
+        if (pluginValue.isEmpty()) {
+            return;
+        }
+        final int chances = pluginValue.get().asInt();
+        // Check chances and fire the lightning
+        if (chances == 0 || random.nextInt(100_000) < chances) {
             entity.getWorld().strikeLightning(entity.getLocation());
             entity.getWorld().playSound(entity.getLocation(), Sound.ENTITY_LIGHTNING_BOLT_IMPACT, SoundCategory.AMBIENT, 1.0f, 1.0f);
         }
