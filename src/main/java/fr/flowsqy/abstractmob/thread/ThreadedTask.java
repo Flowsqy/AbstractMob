6a3806f2ae2b79f0431fe7a22eff192b91ff2fea
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2022-01-22 12:23:39 +0100

    Split UpdaterTask to use ThreadedTask for other things

---
+++ src/main/java/fr/flowsqy/abstractmob/thread/ThreadedTask.java
@@ -0,0 +1,69 @@
+package fr.flowsqy.abstractmob.thread;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class ThreadedTask {
+
+    private final BlockingQueue<Runnable> queue;
+    private final String name;
+    private volatile Thread thread;
+
+    public ThreadedTask(String name) {
+        queue = new LinkedBlockingQueue<>();
+        this.name = name;
+    }
+
+    /**
+     * Start task, except if it is already
+     */
+    public void start() {
+        if (!isRunning()) {
+            thread = new Thread(() -> {
+                while (!Thread.interrupted()) {
+                    try {
+                        queue.take().run();
+                    } catch (InterruptedException e) {
+                        break;
+                    }
+                }
+            }, name);
+            thread.start();
+        }
+    }
+
+    /**
+     * Stop any running task then start it again
+     */
+    public void restart() {
+        stop();
+        start();
+    }
+
+    /**
+     * Stop task properly
+     */
+    public void stop() {
+        if (thread != null) {
+            thread.interrupt();
+            thread = null;
+        }
+    }
+
+    /**
+     * @return whether task is running or not
+     */
+    public boolean isRunning() {
+        return thread != null;
+    }
+
+    /**
+     * Register a task to run before next loop
+     *
+     * @param runnable task to run
+     */
+    public void queue(Runnable runnable) {
+        queue.add(runnable);
+    }
+
+}
--- src/main/java/fr/flowsqy/abstractmob/updater/UpdaterTask.java
+++ src/main/java/fr/flowsqy/abstractmob/updater/UpdaterTask.java
@@ -1,70 +1,13 @@
 package fr.flowsqy.abstractmob.updater;
 
 import fr.flowsqy.abstractmob.key.Keys;
+import fr.flowsqy.abstractmob.thread.ThreadedTask;
 import org.bukkit.entity.Entity;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-
-public class UpdaterTask {
-
-    private final BlockingQueue<Runnable> queue;
-    private volatile Thread thread;
+public class UpdaterTask extends ThreadedTask {
 
     public UpdaterTask() {
-        queue = new LinkedBlockingQueue<>();
-    }
-
-    /**
-     * Start task, except if it is already
-     */
-    public void start() {
-        if (!isRunning()) {
-            thread = new Thread(() -> {
-                while (!Thread.interrupted()) {
-                    try {
-                        queue.take().run();
-                    } catch (InterruptedException e) {
-                        break;
-                    }
-                }
-            }, "Entity Updater");
-            thread.start();
-        }
-    }
-
-    /**
-     * Stop any running task then start it again
-     */
-    public void restart() {
-        stop();
-        start();
-    }
-
-    /**
-     * Stop task properly
-     */
-    public void stop() {
-        if (thread != null) {
-            thread.interrupt();
-            thread = null;
-        }
-    }
-
-    /**
-     * @return whether task is running or not
-     */
-    public boolean isRunning() {
-        return thread != null;
-    }
-
-    /**
-     * Register a task to run before next loop
-     *
-     * @param runnable task to run
-     */
-    public void queue(Runnable runnable) {
-        queue.add(runnable);
+        super("Entity Updater");
     }
 
     public void saveEntities(Entity... entities) {
